f5a590147b53d0049f29b9fded80818852c97c9d
Jamie Howard <jamiehowardco@gmail.com>
2018-02-27 02:16:43 -0500

    Added style changes

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -1,10 +1,8 @@
 <html>
 <head>
 <title><?php echo $env['BLOG_NAME'];?></title>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/app.css">
 </head>
 <body>
     <div class="container">
-        <div class="row col">
-            <a href="/"><h1><?php echo $env['BLOG_NAME'];?></h1></a>
-        </div>
