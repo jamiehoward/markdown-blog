68e67d8d3866b970e790319089dc1cfe817bc32f
Jamie Howard <jamiehowardco@gmail.com>
2018-02-27 01:39:03 -0500

    Created environment variable function to move to helper later.

--- public/index.php
+++ public/index.php
@@ -2,12 +2,18 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$env = [];
-$file = file(__DIR__ . "/../.env");
-foreach($file as $line) {
-    $item = explode('=', $line);
-    $env[$item[0]] = $item[1];
+function getEnvironmentVariables() {
+    $env = [];
+    $file = file(__DIR__ . "/../.env");
+    foreach($file as $line) {
+        $item = explode('=', $line);
+        $env[$item[0]] = $item[1];
+    }
+
+    return $env;
 }
+
+$env = getEnvironmentVariables();
 
 $blog = new \App\Blog;
 $blog->setDirectory(__DIR__ . "/../notes");
