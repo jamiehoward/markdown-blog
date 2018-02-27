f9b7b8e2c2908d48cc721ad216b4c24c89f6d463
Jamie Howard <jamiehowardco@gmail.com>
2018-02-27 02:58:20 -0500

    Removed the superfluous title on note listing.

--- views/notes/list.php
+++ views/notes/list.php
@@ -4,9 +4,6 @@
   <a href="/"><h1><?php echo $env['BLOG_NAME'];?></h1></a>
 </div>
 
-<div class="row col">
-    <h2>My posts</h2>
-</div>
 <div class="row">
     <?php $blog->listNotes();?>
 </div>
