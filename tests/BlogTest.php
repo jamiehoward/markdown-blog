7940bbfd49525fdf6290448414638d251f440f87
Jamie Howard <jamiehowardco@gmail.com>
2018-08-31 13:22:46 -0400

    Implemented PHPUnit

---
+++ tests/BlogTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class BlogTest extends TestCase
+{
+	public function testThatDirectoryCanBeSet()
+	{
+		$this->assertTrue(true);
+	}
+}
