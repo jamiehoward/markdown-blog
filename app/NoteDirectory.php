497fe526e811b45ef674f6ba6c0fcfe65469e4bd
Jamie Howard <jamiehowardco@gmail.com>
2018-08-31 14:11:01 -0400

    Added doc blocks to the NoteDirectory class

--- app/NoteDirectory.php
+++ app/NoteDirectory.php
@@ -14,28 +14,49 @@ class NoteDirectory
         }
     }
 
-	public function getLocation()
-	{
-			return $this->location;
-	}
-
-	public function setLocation($location)
-	{
-			$this->location = $location;
-	}
-
-	protected function readNotes()
+    /**
+     * Get the set filepath for this directory
+     *
+     * @return string
+     */
+    public function getLocation()
     {
-        foreach (new \DirectoryIterator ($this->location) as $note) {
-            if ($note->isDot()) continue;
+        return $this->location;
+    }
+
+    /**
+     * Set the directory's filepath
+     *
+     * @param string $location
+     */
+    public function setLocation($location)
+    {
+        $this->location = $location;
+    }
+
+    /**
+     * Read the contents of all notes from the given directory
+     *
+     * @return void
+     */
+    protected function readNotes()
+    {
+        foreach (new \DirectoryIterator($this->location) as $note) {
+            if ($note->isDot()) {
+                continue;
+            }
 
             if ($note->isFile() && $note->getExtension() == 'md') {
                 $this->notes[] = new Note($this->location . "/{$note->getFilename()}");
             }
         }
-
     }
 
+    /**
+     * Return the array of notes
+     *
+     * @return array All notes in the set directory
+     */
     public function notes()
     {
         $this->readNotes();
@@ -43,12 +64,17 @@ class NoteDirectory
         return $this->notes;
     }
 
+    /**
+     * Retrieve a given note by title
+     * @param  string $title
+     * @return mixed Either the note or a failure
+     */
     public function findNoteByTitle($title)
     {
         foreach ($this->notes() as $note) {
             if (strtoupper($note->getTitle(true)) == strtoupper($title)) {
                 return $note;
-            } 
+            }
         }
 
         return false;
