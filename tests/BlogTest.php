c4ff6c01266bbf742ee4e6499a3e1fc5adf80ecc
Jamie Howard <jamiehowardco@gmail.com>
2018-08-31 13:33:31 -0400

    Added initial blog test

--- tests/BlogTest.php
+++ tests/BlogTest.php
@@ -2,12 +2,17 @@
 
 namespace Tests\Unit;
 
+use App\Blog;
 use PHPUnit\Framework\TestCase;
 
 class BlogTest extends TestCase
 {
 	public function testThatDirectoryCanBeSet()
 	{
-		$this->assertTrue(true);
+		$blog = new Blog;
+		$directory = './notes';
+		$blog->setDirectory($directory);
+
+		$this->assertInstanceOf(\App\NoteDirectory::class, $blog->getDirectory());
 	}
 }
