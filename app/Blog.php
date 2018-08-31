f77ffa6bd6a5b00997f6052e484989c4ae978f10
Jamie Howard <jamiehowardco@gmail.com>
2018-08-31 13:32:21 -0400

    Added docblocks

--- app/Blog.php
+++ app/Blog.php
@@ -13,11 +13,32 @@ class Blog
         }
     }
 
+    /**
+     * Retrieve the blog directory
+     *
+     * @return NoteDirectory
+     */
+    public function getDirectory()
+    {
+        return $this->directory;
+    }
+
+    /**
+     * Set the directory of the blog by filepath
+     *
+     * @param string $location The filepath to the notes directory
+     * @return void
+     */
     public function setDirectory($location)
     {
         $this->directory = new NoteDirectory($location);
     }
 
+    /**
+     * Echo out a list of notes, excluding "pages"
+     *
+     * @return void
+     */
     public function listNotes()
     {
         echo "<ul>";
@@ -31,11 +52,21 @@ class Blog
         echo "</ul>";
     }
 
+    /**
+     * Determine whether currently within a note page
+     *
+     * @return boolean
+     */
     public function isNotePage()
     {
         return ! empty($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] !== '/';
     }
 
+    /**
+     * Echo out the content of the specified note
+     *
+     * @return void
+     */
     public function displayNote()
     {
         $title = substr(urldecode($_SERVER['REQUEST_URI']),1);
