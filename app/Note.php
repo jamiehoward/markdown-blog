15275198e66e2ee92b35957dafb2b7081402c285
Jamie Howard <jamiehowardco@gmail.com>
2018-08-31 14:05:46 -0400

    Added docblocks

--- app/Note.php
+++ app/Note.php
@@ -16,11 +16,21 @@ class Note
         }
     }
 
+    /**
+     * Get the set location of the note
+     *
+     * @return string The filepath to the note file
+     */
     public function getLocation()
     {
         return $this->location;
     }
 
+    /**
+     * Set the location of the note
+     *
+     * @param string $location The filepath to the note
+     */
     public function setLocation($location)
     {
         if (file_exists($location)) {
@@ -30,11 +40,22 @@ class Note
         }
     }
 
+    /**
+     * Retrieve the contents from the note
+     *
+     * @return string The note's contents
+     */
     public function getContents()
     {
         return file_get_contents($this->location);
     }
 
+    /**
+     * Retrieve the note's title
+     *
+     * @param  boolean $format Whether to remove the preceding #
+     * @return string
+     */
     public function getTitle($format = false)
     {
         $title = trim(file($this->location)[0]);
@@ -42,12 +63,23 @@ class Note
         return ($format) ? substr($title, 2) : $title;
     }
 
+    /**
+     * Retrieve the URI element for the note
+     *
+     * @return string
+     */
     public function getLink()
     {
         $link = urlencode($this->getTitle(true));
+
         return "/$link";
     }
 
+    /**
+     * Determine whether the note is a page or not
+     *
+     * @return boolean
+     */
     public function isPage()
     {
         return in_array(strtolower($this->getTitle(true)), $this->pages);
